d2e2e13b3968f52170d0d9bfdfa36c098f8b3692
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-12-08 16:02:42 +0100

    compiles again

--- src/response.rs
+++ src/response.rs
@@ -18,16 +18,16 @@ pub enum Response {
 	#[response(status = 400, content_type = "json")]
 	BadRequest(Option<S>),
 	/// Internal Server Error - 500
-	#[response(status = 500, content_type = "json"))]
+	#[response(status = 500, content_type = "json")]
 	InternalServerError(Option<S>),
 	/// Forbidden - 403
-	#[response(status = 403, content_type = "json"))]
+	#[response(status = 403, content_type = "json")]
 	Forbidden(Option<S>),
 	/// No Content - 204
-	#[response(status = 204, content_type = "json"))]
+	#[response(status = 204, content_type = "json")]
 	NoContent(Option<S>),
 	/// Not Found - 404
-	#[response(status = 404, content_type = "json"))]
+	#[response(status = 404, content_type = "json")]
 	NotFound(Option<S>),
 }
 
