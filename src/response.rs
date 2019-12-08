a36bfb9c4b920ef780acd7b94665cb8d6726c62a
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-12-08 15:07:02 +0100

    enum responses done

--- src/response.rs
+++ src/response.rs
@@ -1,8 +1,13 @@
-
 use rocket::response::Responder;
+use rocket_contrib::json::Json;
 
-// dodelam
-
+/// describes possible responses
 pub enum Response {
-	
+	pub Ok(Json),
+	pub BadRequest,
+	pub InternalServerError,
+	pub Forbidden,
+	pub NoContent,
 }
+
+
